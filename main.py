11eaf00e029869059d127019e19fb88b348b2574
Posen <posen2101024@gmail.com>
2020-07-03 12:15:39 +0800

    init main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+
+from PIL import Image
+
+from termcolor import colored
+
+from os import walk
+
+from os import makedirs
+
+import argparse
+
+def listImages(root_dir):
+
+	return ["{}/{}".format(path, file) for path, _, files in walk(root_dir) for file in files]
+
+def convertImages(images, save_dir, suffix):
+
+	for image in images:
+
+		path, file = image.rsplit("/", 1)
+
+		path = "/".join([save_dir, *path.split("/", 1)[1:]])
+
+		file = "{}.{}".format(file.rsplit(".", 1)[0], suffix)
+
+		output = "{}/{}".format(path, file)
+
+		makedirs(path, exist_ok = True)
+
+		try:
+
+			Image.open(image).save(output)
+
+			print(colored("{} --> {}".format(image, output), "green"))
+
+		except:
+
+			print(colored("{} --> {}".format(image, output), "red"))
+
+def main(args):
+
+	images = listImages(root_dir = args.root_dir)
+
+	convertImages(images = images, save_dir = args.save_dir, suffix = args.suffix)
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("root_dir", type = str)
+
+	parser.add_argument("save_dir", type = str, default = "result", nargs = "?")
+
+	parser.add_argument("--suffix", type = str, default = "png")
+
+	args = parser.parse_args()
+
+	main(args)
